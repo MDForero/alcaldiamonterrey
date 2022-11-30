2f9b12915a9bd50b297e97c80301627c52468e46
MDForero <mforeroroldan@gmail.com>
2022-11-30 14:27:57 -0500

    navbar creado y optimizado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Layout from './components/Layout';
+import Inicio from './pages/Inicio';
+import Empresas from './pages/Empresas';
+import Contacto from './pages/Contacto';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/' element={<Layout/>}>
+          <Route index element={<Inicio/>}/>
+          <Route path='empresas' element={<Empresas/>}/>
+          <Route path='contacto' element={<Contacto/>}/>
+        </Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Layout.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Container, Nav, Navbar } from 'react-bootstrap'
+import { Link, NavLink, Outlet } from 'react-router-dom'
+import logo from "../images/logoalcaldia.png"
+
+const Layout = () => {
+    
+    return (
+        <section>
+            <header style={{display:"flex", justifyContent:"center", alignContent:"center", background:"#037ccb"}}>
+                <figure style={{width:"400px"}}>
+                    <img src={logo} width="100%"/>
+                </figure>
+            </header>
+            <Navbar style={{background:"#037ccb"}} expand="lg" sticky='top'>
+                <Container fluid>
+                    <Navbar.Brand href="#"></Navbar.Brand>
+                    <Navbar.Toggle aria-controls="navbarScroll" />
+                    <Navbar.Collapse id="navbarScroll">
+                        <Nav
+                            className="ms-auto my-2 my-lg-0"
+                            style={{ maxHeight: '100px', fontSize:"2rem" }}
+                            navbarScroll
+                        >
+                            <NavLink className="nav-link" to="/" >Inicio</NavLink>
+                            <NavLink className="nav-link" to="empresas">Empresas</NavLink>
+                            <NavLink className="nav-link" to="contacto">Contacto</NavLink>
+                        </Nav>
+
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            <Outlet />
+        </section>
+    )
+}
+
+export default Layout
