5e440b42eeceff909ff0979f75a341cdaae57ccf
Michael Forero <mforeroroldan@gmail.com>
2022-12-05 09:58:12 -0500

    muestra de comercio creado

--- src/components/Comercio.js
+++ src/components/Comercio.js
@@ -1,9 +1,12 @@
+import { faFacebook, faInstagram, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useState } from 'react'
-import { Button, Modal } from 'react-bootstrap';
+import { Button, Carousel, CarouselItem, Modal } from 'react-bootstrap';
 import { Link } from 'react-router-dom'
 
 const Comercio = ({ data }) => {
-    const { img, nombre } = data
+    const { img, nombre, carousel, ubicacion, redes } = data
+    const { instagram, whatsapp, facebook } = redes
     const [show, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
@@ -13,11 +16,11 @@ const Comercio = ({ data }) => {
     return (
         <div>
             <Link variant="primary" onClick={handleShow} style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-                    <figure className="figure" >
-                        <img src={img} alt={nombre} width="100%" style={{ position: "relative" }} className="img" />
-                    </figure>
-                    <h1 style={{ position: "absolute" }}>{nombre}</h1>
-                
+                <figure className="figure" >
+                    <img src={img} alt={nombre} width="100%" style={{ position: "relative" }} className="img" />
+                </figure>
+                <h1 style={{ position: "absolute" }}>{nombre}</h1>
+
 
             </Link>
 
@@ -25,7 +28,58 @@ const Comercio = ({ data }) => {
                 <Modal.Header closeButton>
                     <Modal.Title>Modal heading</Modal.Title>
                 </Modal.Header>
-                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                <Modal.Body>
+                    <section style={{ height: "200px", overflow: "hidden" }}>
+                        <Carousel fade>
+                            {carousel.map((element) => <Carousel.Item interval={1000}>
+                                <img
+                                    className="d-block"
+                                    src={element}
+                                    height="300px"
+                                    width="100%"
+                                    alt=""
+                                    style={{ objectFit: "cover" }}
+                                />
+                            </Carousel.Item>)}
+                        </Carousel>
+                    </section>
+                    <section style={{ display: "grid", gridTemplateColumns: "1fr 1fr", margin: "50px 0px", gap: "10px", textAlign: "justify" }}>
+                        <div className='container'>
+                            <div className="gap-10">
+                                <div className='contorno container'>
+
+                                    <h1>Descripción</h1>
+                                    <p>orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco</p>
+                                </div>
+                                <div className='contorno container'>
+                                    <h2>Horarios de atención</h2>
+                                    <dl>
+                                        <dt>Lunes-Viernes</dt>
+                                        <dl>9:00am - 9:00pm</dl>
+                                        <dt>Sabados y Domingos</dt>
+                                        <dl>9:00am - 9:00pm</dl>
+                                    </dl>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="container">
+                            <div className='container gap-10'>
+                                <div className='contorno container'>
+                                    <h2 style={{textAlign:"center"}} >Redes sociales</h2>
+                                    <ul style={{ display: "flex", listStyle: "none", justifyContent: "space-around" }}>
+                                        <li><a href={facebook}><FontAwesomeIcon icon={faFacebook} size="3x" /></a></li>
+                                        <li><a href={instagram}><FontAwesomeIcon icon={faInstagram} size="3x" /></a></li>
+                                        <li><a href={whatsapp}><FontAwesomeIcon icon={faWhatsapp} size="3x" /></a></li>
+                                    </ul>
+                                </div>
+                                <div className='contorno'>
+
+                                <iframe  src={ubicacion} width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={handleClose}>
                         Close
--- src/images/data.js
+++ src/images/data.js
@@ -1,8 +1,18 @@
 import local from "../images/comercios.jpg"
+import carousel1 from "../images/carrousel1.jpg"
+import carousel2 from "../images/carrousel2.jpg"
+import carousel3 from "../images/carrousel3.jpg"
 export const comercios = [
     {
         img:local,
-        nombre:"lorem"
+        nombre:"lorem",
+        carousel:[carousel1, carousel2, carousel3],
+        ubicacion:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d496.9205048210907!2d-72.89789397223241!3d4.878547399098159!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e6ad319b09be25d%3A0x9594b966c7649430!2sDe%20La%20Casa%20-%20Sabor%20Artesanal!5e0!3m2!1ses!2sco!4v1670179699202!5m2!1ses!2sco",
+        redes:{
+               facebook:"",
+               instagram:"",
+               whatsapp:"", 
+            }
         
     }
 ]
