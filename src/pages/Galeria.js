f1b038b02509a0776dd520b6ceb66c8e8476d30b
Michael Forero <mforeroroldan@gmail.com>
2022-12-08 11:42:12 -0500

    galeria diseñada e implementada

--- src/pages/Galeria.js
+++ src/pages/Galeria.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import logo from "../images/corazon.png"
+import galeria from "../images/Galeria/galeria"
 
 const Galeria = () => {
   return (
-    <div><main className='preview'>
+    <div><main className='preview' id="galeria">
       <div className='overlay'>
         <img src={logo} />
       </div>
     </main>
+    <section className='galeria'>
+      {galeria.map((element)=><img src={element} className="gallery__img" style={{margin:"0px auto"}}/>)}
+    </section>
     </div>
   )
 }
