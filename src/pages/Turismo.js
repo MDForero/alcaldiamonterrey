9565a67323c80ffe17f75dd032c7832b85827d54
Michael Forero <mforeroroldan@gmail.com>
2022-12-12 12:55:09 -0500

    turismo actualizado

--- src/pages/Turismo.js
+++ src/pages/Turismo.js
@@ -15,8 +15,8 @@ const Turismo = () => {
         <article className='turismo' id="tua">
           <div className='overlay-turismo'>
             <div>
-              <h3>Lorem</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+              <h3>Río Tua</h3>
+              <p>Fuente hídrica representativa de la región, de color cristalino, permitiendo observar los colores de los ecosistemas acuáticos, además de tener caudales tranquilos propicios para su uso como balneario.</p>
             </div>
           </div>
         </article>
@@ -24,20 +24,39 @@ const Turismo = () => {
         <article className='turismo' id="roca">
           <div className='overlay-turismo'>
             <div>
-              <h3>Lorem</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+              <h3>CAÑO LA ROCA</h3>
+              <p>Se encuentra dentro del territorio urbano, consiste en la formación bajo una roca de una piscina natural, esta cuenta con infraestructura en guadua, un pequeño puente así como un lugar adecuado construido en piedra, para reuniones recreación y turismo.</p>
+            </div>
+          </div>
+        </article>
+        
+        <article className='turismo' id="puente">
+          <div className='overlay-turismo'>
+            <div>
+              <h3>PUENTE TABLA</h3>
+              <p>Puente colgante que hace porte un reconocido camino real histórico, bajo el puente se encuentra Un fragmento del rio túa, que está colmado de vegetación nativo, nacederos, aguas cristalinos.</p>
             </div>
           </div>
         </article>
 
-        <article className='turismo' id="caño">
+        <article className='turismo' id="sol">
           <div className='overlay-turismo'>
             <div>
-              <h3>Lorem</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+              <h3>Piedra del sol</h3>
+              <p>Mirador natural ubicado en una los puntos más elevados de nuestro municipio, ideal para hacer avistamiento del amanecer llanero, senderismo, excursionismo; así como poro conocer un poco de nuestro historio.</p>
             </div>
           </div>
         </article>
+
+        <article className='turismo' id="pozo">
+          <div className='overlay-turismo'>
+            <div>
+              <h3>POZO DEL AMOR:</h3>
+              <p>En medio del espesor del bosque nativo se encuentran las refrescantes aguas cristalinas, presentándose mediante varias cascadas de manera semejante a un tobogán. Es un sitio que permite a los turistas disfrutar de actividades como senderismo, avistamiento de aves, excursionismo, y regocijarse bajo estas encantadoras caídas de agua.</p>
+            </div>
+          </div>
+        </article>
+
       </section>
     </div>
   )
